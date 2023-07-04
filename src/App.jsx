5daa75bb82ba870ec41e2b84fc9922d0222f90d7
yaseeen96 <syedyaseen1539@gmail.com>
2023-07-05 04:34:26 +0530

    comment testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ const App = () => {
 
         <Tech />
         <Works />
-        <Feedbacks />
+        {/* <Feedbacks /> */}
         <div className="relative z-0">
           <Contact />
           <StarsCanvas />
